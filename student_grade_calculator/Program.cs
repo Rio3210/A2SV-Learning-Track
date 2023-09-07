bd177961bcc24f07d8a6b4dd522db3584374e672
Surafel Workayehu <surafelwork32@gmail.com>
2023-09-07 21:40:28 +0300

    handle grade with floating number

--- student_grade_calculator/Program.cs
+++ student_grade_calculator/Program.cs
@@ -5,24 +5,30 @@
         string? name = Console.ReadLine();
         Console.WriteLine("Please enter the number of subjects you have taken: ");
         
-        Dictionary<string, int> subjects = new Dictionary<string, int>();
+        Dictionary<string, float> subjects = new Dictionary<string, float>();
         int num_subjects = Convert.ToInt32(Console.ReadLine());
         for (int i = 0; i < num_subjects; i++){
             Console.WriteLine("Please enter the name of subject " + (i+1) + ": ");
             string? subject = Console.ReadLine();
             Console.WriteLine("Please enter the grade you obtained for " + subject + ": ");
-            int grade = Convert.ToInt32(Console.ReadLine());
-            subjects.Add(subject, grade);
+            float grade = Convert.ToSingle(Console.ReadLine());
+            if (grade >= 0 && grade <= 100){
+                subjects.Add(subject, grade);
+            } else {
+                Console.WriteLine("Invalid grade entered. Please enter a grade between 0 and 100.");
+                i--;
+            }
+            
         }
         
-        int total = 0;
-        foreach (KeyValuePair<string, int> subject in subjects){
+        float total = 0;
+        foreach (KeyValuePair<string, float> subject in subjects){
             total += subject.Value;
         }
-        int average = total / num_subjects;
+        float average = total / num_subjects;
         
         Console.WriteLine("Name: " + name);
-        foreach (KeyValuePair<string, int> subject in subjects){
+        foreach (KeyValuePair<string, float> subject in subjects){
             Console.WriteLine(subject.Key + ": " + subject.Value);
         }
         Console.WriteLine("Average grade: " + average);
