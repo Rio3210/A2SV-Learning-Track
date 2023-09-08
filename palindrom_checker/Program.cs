42e5c3bbaabf7026a950fddba1d7884d3e57b848
Surafel Workayehu <surafelwork32@gmail.com>
2023-09-08 11:04:16 +0300

    create palindrom checker

---
+++ palindrom_checker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Program {
+    static void Main(string[] args){
+        Console.WriteLine("Please enter a string: ");
+        string? word = Console.ReadLine().Trim();
+        string input = Regex.Split(word, @"\s+")[0];
+        string reversed = "";
+        foreach (char c in input){
+            if (char.IsLetter(c)){
+                reversed = c + reversed;
+            }
+        }
+        if (input.ToLower() == reversed.ToLower()){
+            Console.WriteLine("This is a palindrome!");
+        } else {
+            Console.WriteLine("This is not a palindrome!");
+        }
+    }
+}
